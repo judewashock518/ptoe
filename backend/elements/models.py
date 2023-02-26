ece172d8c2a551d941a390a18a6fb5e040e14694
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-26 16:15:36 -0500

    migrated 'elements' table, created model

--- backend/elements/models.py
+++ backend/elements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Element(models.Model):
+    atomic_number = models.IntegerField()
+    symbol = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    group = models.IntegerField()
+    atomic_mass = models.DecimalField(max_digits=8, decimal_places=2)
+    more_info = models.CharField(max_length=255)
