c9bae04a80fc56257bd5f91a8450f9a9a946520f
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-28 13:16:38 -0500

    created urls path for 'students' api

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('api/elements/', include('elements.urls'))
+    path('api/elements/', include('elements.urls')),
+    path('api/students/', include('students.urls'))
 ]
