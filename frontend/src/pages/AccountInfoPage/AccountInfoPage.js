aa1e235d9c99dde5dffee1649dbfb2b3aa7fbe18
JW <114599894+judewashock518@users.noreply.github.com>
2023-03-09 16:44:28 -0500

    started account info page

--- frontend/src/pages/AccountInfoPage/AccountInfoPage.js
+++ frontend/src/pages/AccountInfoPage/AccountInfoPage.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
+import './AccountInfoPage.css'
+import axios from 'axios';
 
-const AccountInfoPage = (props)=>{
+const AccountInfoPage = ()=>{
+    // const [info, setInfo] = useState([]);
+    // const [user, token] = useAuth();
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       const result = await axios.get('http://127.0.0.1:8000/api/students/detail/', 
+    //       {headers:{Authorization:"Bearer " +token}});
+    //       console.log(result.data);
+    //       setInfo(result.data);
+    //     };
+    //     fetchData();
+    // }, []);
+    
     return(
         <div>
-            Account Info Page
+
         </div>
     )
 }
